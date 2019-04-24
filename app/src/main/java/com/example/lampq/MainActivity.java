a0ce0241ff543db5b243795d0a895ea2aa70ba9d
Aan Sanova <aansanova.14116175@student.itera.ac.id>
2019-04-24 22:56:04 +0700

    init

---
+++ app/src/main/java/com/example/lampq/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.lampq;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.main_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,
+                    new BeritaFragment()).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_berita:
+                            selectedFragment = new BeritaFragment();
+                            break;
+                        case R.id.nav_tempat:
+                            selectedFragment = new TempatFragment();
+                            break;
+                        case R.id.nav_wisata:
+                            selectedFragment = new WisataFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
+
+
